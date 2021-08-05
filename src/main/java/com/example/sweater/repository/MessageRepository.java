449d2e0276ec20b3d4a9396eb1cba0c04aa6c4b9
Andrejs Breidaks <andrejs.breidaks@inbox.lv>
2021-08-05 10:42:55 +0300

    add start

---
+++ src/main/java/com/example/sweater/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.example.sweater.repository;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message, Integer> {
+}
